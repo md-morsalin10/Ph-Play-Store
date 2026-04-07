60cb959198c554094c024988c4657d902f64be87
md-morsalin10 <mdmorsalinofficial10@gmail.com>
2026-04-07 22:53:31 +0600

    raect file created and tailwind, daisyui, react router are installed

---
+++ src/main.jsx
@@ -0,0 +1,31 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import { createBrowserRouter, Router } from 'react-router';
+import { RouterProvider } from 'react-router/dom';
+import RootLayout from './Layout/RootLayout';
+import HomePage from './pages/HomePage/HomePage';
+import AppPage from './pages/AppPage/AppPage';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: RootLayout,
+    children:[
+      {
+        index: true,
+        Component: HomePage
+      },
+      {
+        path:"/app",
+        Component: AppPage
+      }
+    ]
+  },
+]);
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+)
